bfb467cd2a0299a76ea8c05b59e1c8bfbe442048
Ndichu James <ndichujames2030@gmail.com>
2024-01-06 12:33:51 +0300

    doen with the bio section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,21 +2,21 @@ import "./project.css";
 import mashamba_screen_shot from "../assets/mashamba.png";
 const Project = () => {
   return (
-    <div className="project">
-      <img src={mashamba_screen_shot} alt="" />
-      <div className="project_details">
-        <div>
+    <>
+      <a href="http://" className="project">
+        <p>
           <h3>Mashamba</h3>
-        </div>
-        <hr />
-        <div>Description:</div>
-        <div>
-          <a href="" target="blank">
-            Visit Mashamba
-          </a>
-        </div>
-      </div>
-    </div>
+        </p>
+        <figure>
+          <figcaption className="project_details">
+            <p>Surveyor's digital document assistant.</p>
+            <p>Status: <b>Ongoing Project.</b></p>
+          </figcaption>
+          <img src={mashamba_screen_shot} alt="" />
+        </figure>
+      </a>
+    </>
   );
 };
+
 export default Project;
