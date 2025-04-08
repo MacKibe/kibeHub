a16f60a0bf65a7a763347fc2cbef4513ba8b830f
Mackibe <ndichujames2030@gmail.com>
2025-04-08 09:40:21 +0300

    Done with Hero section

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,10 +1,28 @@
 import Navbar from "../components/Navbar";
 import Logo from "../components/Logo";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
+import { useState } from "react";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    // Ensures the state is toggled correctly.
+    setIsOpen((prev) => !prev);
+  };
   return (
     <header className="flex justify-between items-center bg-background-2 px-30 py-4 sticky top-0 text-lg">
       <Logo />
-      <Navbar />
+      <Navbar direction="row" />
+      {/* Mobile Menu Button */}
+      <button onClick={toggleMenu} className="md:hidden focus:outline-none">
+          {isOpen ? (
+            <MdClose style={{ color: "var(--text)" }} size={40} />
+          ) : (
+            <GiHamburgerMenu style={{ color: "var(--text)" }} size={40} />
+          )}
+      </button>
     </header>
   );
 };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,24 @@
+import { LiaDownloadSolid } from "react-icons/lia";
+import { FaWandMagicSparkles } from "react-icons/fa6";
+import { TbBracketsAngle } from "react-icons/tb";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="flex flex-col text-center justify-center text-xl space-y-8 h-[90dvh] w-[45%] mx-auto">
+      <p className="flex gap-4 items-center mx-auto"><TbBracketsAngle />Software Developer</p>
+      <h1 className="text-6xl font-bold">
+        Hi, I'm <b className="text-primary">Ndichu James Kiberenge👋</b>
+      </h1>
+      <p>Crafting clean and scalable web solutions</p>
+      <p className="w-[80%] mx-auto">
+        I'm a passionate software developer with a knack for building functional
+        and user-friendly web applications.
+      </p>
+      <div className="flex justify-evenly space-x-4">
+        <a href="#projects" className="flex gap-4 items-center bg-primary hover:bg-tertiary text-white px-6 py-3 rounded-2xl"><FaWandMagicSparkles /> View projects </a>
+        <a href="#contactMe" className="flex gap-4 items-center text-primary hover:bg-tertiary px-6 py-3 border-2 primary  rounded-2xl"><LiaDownloadSolid /> Contact Me </a>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
